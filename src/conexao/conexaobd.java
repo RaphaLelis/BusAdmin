ae0ce34e113b3fcdd095b73ab24ad02768821bb9
Raphael Menezes Lelis <raphalelis@outlook.com>
2020-09-22 09:26:40 -0300

    Initial commit

---
+++ src/conexao/conexaobd.java
@@ -0,0 +1,28 @@
+package conexao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Rapha
+ */
+public class conexaobd {
+    public static Connection getConnection() throws SQLException{
+	try{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		JOptionPane.showMessageDialog(null, "Conectado ao banco de dados");
+		return DriverManager.getConnection("jdbc:mysql://localhost:3306/login", "root", "");
+                
+	}
+        catch (ClassNotFoundException e) {
+                JOptionPane.showMessageDialog(null, "Não conseguimos conectar o banco de dados!");
+                throw new SQLException(e.getMessage());
+	}
+}
+}
+    
+
